6a5ee954054126c5cfb92fec0acfb7cbc091bfcc
Sadick Mumin <102040809+sadick22@users.noreply.github.com>
2026-06-02 21:57:47 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FifteenFramework from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FifteenFramework />
+  </React.StrictMode>
+);
